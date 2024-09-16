eabfc3d64d4ba2bc45347abbfe0d2f59878c32ea
임채령 <sa990422@gmail.com>
2024-09-16 17:01:39 +0900

    [Gold IV] Title: 오큰수, Time: 900 ms, Memory: 147932 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        int[] result = new int[N];
+        Stack<Integer> stack = new Stack<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                result[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            result[stack.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(result[i]).append(" ");
+        }
+
+        System.out.println(sb);
+        
+    }
+
+}
